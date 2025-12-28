12346cc8791bad77eaaa811dbde6b1c71f61f8a8
harsshhhgit <harsshhhs@yahoo.com>
2025-12-28 21:22:41 +0530

    fix: fixed basics

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,19 @@
 import { defineConfig, fontProviders } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
+import rehypePrettyCode from 'rehype-pretty-code';
+import rehypeCodeTitles from 'rehype-code-titles';
+import rehypeAutolinkHeadings from 'rehype-autolink-headings';
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
+  markdown: {
+    shikiConfig: {
+      // https://shiki.style/themes
+      theme: 'github-dark-default',
+    },
+  },
   experimental: {
     fonts: [
       {
@@ -12,7 +22,7 @@ export default defineConfig({
         cssVariable: "--font-roboto",
         provider: fontProviders.google(),
         // Default included:
-        // weights: [400] ,
+        // weights: [400, 500, 600, 700, 800] ,
         // styles: ["normal", "italics"],
         // subsets: ["cyrillic-ext", "cyrillic", "greek-ext", "greek", "vietnamese", "latin-ext", "latin"],
         // fallbacks: ["sans-serif"],
@@ -33,7 +43,7 @@ export default defineConfig({
         cssVariable: "--font-open-sans",
         provider: fontProviders.google(),
         // Specify weights that are actually used
-        // weights: [400, 600],
+        weights: [400, 500, 600, 700, 800],
         // Specify styles that are actually used
         // styles: ["normal", "italics"],
         // Download only font files for characters used on the page
@@ -41,7 +51,24 @@ export default defineConfig({
       }
     ]
   },
+
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+
+  image: {
+    domains: ["github.com", "cloudinary.com"],
+  },
+
+  integrations: [mdx({
+    rehypePlugins: [
+      rehypeCodeTitles,
+      [rehypeAutolinkHeadings, { properties: { className: ['anchor'] } }]
+      // [rehypePrettyCode,
+      //   {
+      //     theme: 'github-dark',
+      //   }
+      // ]
+    ]
+  })]
 });
